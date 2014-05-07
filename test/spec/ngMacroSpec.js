6606ef835a5d9a7a41ccb17cf9435af7fd88160c
unknown <a505194@LA909508.dmn1.fmr.com>
2014-05-07 16:48:31 +0100

    loading in AngularJS module and injecting in our controller and scope

--- test/spec/ngMacroSpec.js
+++ test/spec/ngMacroSpec.js
@@ -3,3 +3,30 @@ describe("A suite", function() {
     expect(true).toBe(true);
   });
 });
+
+describe("ngMacro", function(){
+    //define our scope and controller
+    var $scope, ctrl;
+
+    //before each test
+     beforeEach(function(){
+        //load our module
+        module('ngMacro');
+        //inject our scope and controller with that of our 'macroCtrl'
+        inject(function ($rootScope, $controller) {
+
+              $scope = $rootScope.$new();
+
+              ctrl = $controller('macroCtrl', {
+                $scope: $scope
+              });
+          });
+    });
+
+    it("Check ng-macro module exists", function(){
+
+        expect($scope.goals.current.title).toBe("Mainteance");
+    });
+
+
+});
